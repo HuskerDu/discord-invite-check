0df0f7d7a33386529088194788ef8eefe897e8b8
HuskerDu <adithyajr11@gmail.com>
2017-09-03 10:53:55 +0530

    Organised code into a function

---
+++ checklink.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+import time
+import link
+
+check = webdriver.Chrome('/usr/bin/chromedriver')
+
+
+def check_invite(b):
+    check.get(b)
+    time.sleep(1)  # wait for window to completely load
+    try:
+        check.find_element_by_css_selector(
+            '.auth-form.form.deprecated.auth-form-invite.register')  # check to see if link is valid
+        return '{} {}'.format('Link found at', b)
+    except:
+        return 0
+
+
+yes = 1
+while yes:
+    invite = link.create()
+    ok = check_invite(invite)
+    if ok:
+        print(ok)
+        yes = 0
+check.close()
